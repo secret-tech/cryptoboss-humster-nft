372795c320b45ae992b6e2ade046e14c22c01c9b
Degtyaruk Andrey <hlogeon1@gmail.com>
2022-09-07 17:12:42 +0300

    init

---
+++ contracts/CryptoBossHamsters.sol
@@ -0,0 +1,68 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+
+contract CryptoBossHamsters is ERC721, ERC721URIStorage, Pausable, Ownable {
+    using Counters for Counters.Counter;
+
+    Counters.Counter private _tokenIdCounter;
+    uint256 private constant NUMBER_OF_UNIQUE_TOKENS = 20;
+    string private baseURI =
+        "https://ipfs.io/ipfs/QmcBboh2gJBUFF5M3KJZZfJk2TbAJwwLtpPPnfSc77YfW2/";
+
+    constructor() ERC721("CryptoBoss Hamsters", "CRYBH") {}
+
+    function _baseURI() internal view override returns (string memory) {
+        return baseURI;
+    }
+
+    function pause() public onlyOwner {
+        _pause();
+    }
+
+    function unpause() public onlyOwner {
+        _unpause();
+    }
+
+    function safeMint(address to) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override whenNotPaused {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        uint256 tokenImageId = tokenId % NUMBER_OF_UNIQUE_TOKENS;
+        string memory uri = string.concat(
+            baseURI,
+            Strings.toString(tokenImageId)
+        );
+        return string.concat(uri, ".json");
+    }
+}
